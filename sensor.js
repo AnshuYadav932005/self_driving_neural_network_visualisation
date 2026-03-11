09ee0ab9f6e7cf70dc32ba17ca43021556fdea00
anshu_yadav <anshuy.932005@gmail.com>
2026-03-12 00:42:29 +0530

    neural network

--- sensor.js
+++ sensor.js
@@ -1,48 +1,66 @@
 class Sensor{
     constructor(car){
         this.car=car;
-        this.rayCount=3;
-        this.rayLength=100;
-        this.raySpread=Math.PI/4;
-        
+        this.rayCount=5;
+        this.rayLength=150;
+        this.raySpread=Math.PI/2;
+
         this.rays=[];
-         this.readings=[];
+        this.readings=[];
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         this.#castRays();
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
             this.readings.push(
-                this.#getReading(this.rays[i],roadBorders)
+                this.#getReading(
+                    this.rays[i],
+                    roadBorders,
+                    traffic
+                )
             );
         }
     }
 
-    #getReading(ray,roadBorders){
-        // let touches=[];
+    #getReading(ray,roadBorders,traffic){
+        let touches=[];
 
-        // for(let i=0;i<roadBorders.length;i++){
-        //     const touch=getIntersection(
-        //         ray[0],
-        //         ray[1],
-        //         roadBorders[i][0],
-        //         roadBorders[i][1]
-        //     );
-        //     if(touch){
-        //         touches.push(touch);
-        //     }
-        // }
+        for(let i=0;i<roadBorders.length;i++){
+            const touch=getIntersection(
+                ray[0],
+                ray[1],
+                roadBorders[i][0],
+                roadBorders[i][1]
+            );
+            if(touch){
+                touches.push(touch);
+            }
+        }
 
-        // if(touches.length==0){
-        //     return null;
-        // }else{
-        //     const offsets=touches.map(e=>e.offset);
-        //     const minOffset=Math.min(...offsets);
-        //     return touches.find(e=>e.offset==minOffset);
-        // }
+        for(let i=0;i<traffic.length;i++){
+            const poly=traffic[i].polygon;
+            for(let j=0;j<poly.length;j++){
+                const value=getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
+            }
+        }
+
+        if(touches.length==0){
+            return null;
+        }else{
+            const offsets=touches.map(e=>e.offset);
+            const minOffset=Math.min(...offsets);
+            return touches.find(e=>e.offset==minOffset);
+        }
     }
-
 
     #castRays(){
         this.rays=[];
@@ -50,8 +68,9 @@ class Sensor{
             const rayAngle=lerp(
                 this.raySpread/2,
                 -this.raySpread/2,
-                this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
-            );
+                this.rayCount==1?0.5:i/(this.rayCount-1)
+            )+this.car.angle;
+
             const start={x:this.car.x, y:this.car.y};
             const end={
                 x:this.car.x-
@@ -66,13 +85,13 @@ class Sensor{
     draw(ctx){
         for(let i=0;i<this.rayCount;i++){
             let end=this.rays[i][1];
+            if(this.readings[i]){
+                end=this.readings[i];
+            }
 
-            //here we need to have to readings array
-            
-            //we drawing this to indicate the ray that are being used in our project
-            ctx.beginPath();//reset this path i am starting a new shape to draw
-            ctx.lineWidth=2;//set line width
-            ctx.strokeStyle="yellow";//color
+            ctx.beginPath();
+            ctx.lineWidth=2;
+            ctx.strokeStyle="yellow";
             ctx.moveTo(
                 this.rays[i][0].x,
                 this.rays[i][0].y
@@ -83,7 +102,6 @@ class Sensor{
             );
             ctx.stroke();
 
-            //if there is to indicate the intersection of the sensor ray with something else
             ctx.beginPath();
             ctx.lineWidth=2;
             ctx.strokeStyle="black";
@@ -97,5 +115,5 @@ class Sensor{
             );
             ctx.stroke();
         }
-    }
+    }        
 }
